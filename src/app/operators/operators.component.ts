13ec68e1926f608bf89a45b2ae53ef762af505e8
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-07-26 21:19:54 -0300

    Operators: filter

--- src/app/operators/operators.component.ts
+++ src/app/operators/operators.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { from, fromEvent } from 'rxjs';
-import { map, delay } from 'rxjs/operators';
+import { from, interval, fromEvent } from 'rxjs';
+import { map, delay, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-operators',
@@ -16,13 +16,26 @@ export class OperatorsComponent implements OnInit {
 
   mapClick() {
     from([1, 2, 3, 4, 5, 6, 7]).pipe(
-      map(i => i*2),
-      map(i => i*10),
+      map(i => i * 2),
+      map(i => i * 10),
       delay(2000)
     ).subscribe(i => console.log(i));
 
     fromEvent(document, 'click').pipe(
-      map((e: MouseEvent) => ({x: e.screenX, y: e.screenY}))
+      map((e: MouseEvent) => ({ x: e.screenX, y: e.screenY }))
     ).subscribe((pos) => console.log(pos));
+  }
+
+  filterClick() {
+    from([1, 2, 3, 4, 5, 6, 7]).pipe(
+      filter(i => i % 2 == 1)
+    ).subscribe(i => console.log(i));
+
+    interval(1000)
+      .pipe(
+        filter(i => i % 2 == 0),
+        map(i => "Value: " + i),
+        delay(1000)
+      ).subscribe(i => console.log(i));
   }
 }
