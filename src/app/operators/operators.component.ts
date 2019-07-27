0acaa73bab2bfbde191d51cd5bf6281f7343d4e3
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-07-26 23:57:39 -0300

    operators: takeWhile and takeUntil

--- src/app/operators/operators.component.ts
+++ src/app/operators/operators.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { from, interval, fromEvent, Observable, Subscription, Subject } from 'rxjs';
-import { map, delay, filter, tap, take, first, last, debounceTime } from 'rxjs/operators';
+import { from, interval, fromEvent, Observable, Subscription, Subject, timer } from 'rxjs';
+import { map, delay, filter, tap, take, first, last, debounceTime, takeWhile, takeUntil } from 'rxjs/operators';
 import { MatRipple } from '@angular/material';
 
 @Component({
@@ -117,5 +117,26 @@ export class OperatorsComponent implements OnInit {
     this.searchEntry$
     .pipe(debounceTime(500))
       .subscribe((s) => console.log(s))
+  }
+
+  takeWhileClick() {
+    interval(500)
+    .pipe(takeWhile((value, index) => (value<5)))
+    .subscribe(
+      (i)=> console.log('takeWhile: ', i),
+      (error) => console.error(error),
+      ()=> console.log('Completed!'));
+  }
+
+  takeUntilSearch() {
+
+    let duetime$ = timer(5000);
+
+    interval(500)
+    .pipe(takeUntil(duetime$))
+    .subscribe(
+      (i)=> console.log('takeWhile: ', i),
+      (error) => console.error(error),
+      ()=> console.log('Completed!'));
   }
 }
